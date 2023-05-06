1e46a24ea1791d27e84bf050fb241b4e583fc3f3
Nhat Nguyen <nhat.nguyen@dal.ca>
2023-05-05 21:25:15 -0300

    Add existing files

---
+++ src/test/java/JPALearn/JpaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package JPALearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
